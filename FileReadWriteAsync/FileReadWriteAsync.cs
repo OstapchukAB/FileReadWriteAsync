ca28dc0b2d5fa5f4b481f31931095d1bade4b2f1
OstapchukAB <electron.anton@gmail.com>
2023-10-27 23:06:35 +0300

    Add project files.

---
+++ FileReadWriteAsync/FileReadWriteAsync.cs
@@ -0,0 +1,46 @@
+﻿namespace StreamReadWriteAsync;
+public class FileReadWriteAsync
+{
+    public static void Main()
+    {
+
+    }
+    //Читаем и пишем асинхронно из файла и в файл
+    //условие записи: символы схраняют порядок и принадлежат ascii от a..z
+    public async Task<int> TransferSymbolsAsync(Stream source, Stream destination)
+    {
+        byte[] buffer = new byte[1];
+        try
+        {
+            var cntOUT = 0;
+            for (long i = 0; i < source.Length; i++)
+            {
+                checked
+                {
+                    await source.ReadAsync(buffer);
+                    {
+                        if (buffer[0]>=0x61 && buffer[0]<=0x7a)
+                        {
+                            await destination.WriteAsync(buffer);
+                            cntOUT++;
+                        }
+                    }
+                }
+
+
+            }
+            return cntOUT;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            throw;
+        }
+        finally
+        {
+            destination.Dispose();
+            source.Dispose();
+            
+        }
+    }
+}
---
+++ FileReadWriteAsyncTests/FileReadWriteAsyncTests.cs
@@ -0,0 +1,25 @@
+﻿using StreamReadWriteAsync;
+using Xunit;
+namespace FileReadWriteAsyncTests;
+
+public  class FileReadWriteAsyncTests
+{
+    [Fact]
+    public async Task Test_TransferSymbols()
+    {
+        //Arrange
+        Stream streamIn = new FileStream("test.raw", FileMode.Open);
+        //var lenSreamIn=streamIn.Length;
+        Stream streamOut = new FileStream("testOut.raw", FileMode.Create);
+        var ob = new FileReadWriteAsync();
+
+        //Act
+        var task = ob.TransferSymbolsAsync(streamIn, streamOut);
+        await task;
+        //Assert
+        Assert.False(streamIn.CanRead);
+        Assert.False(streamOut.CanWrite);
+        Assert.True(task.IsCompletedSuccessfully);
+        //Assert.Equal(lenSreamIn, task.Result);
+    }
+}
