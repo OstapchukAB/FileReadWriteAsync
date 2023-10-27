7271abdd2bd09633b9dffbd43d9a32aa6bdbd0bf
OstapchukAB <electron.anton@gmail.com>
2023-10-27 23:40:19 +0300

    Добавил вторую версию TransferSymbols

--- FileReadWriteAsync/FileReadWriteAsync.cs
+++ FileReadWriteAsync/FileReadWriteAsync.cs
@@ -13,13 +13,14 @@ public class FileReadWriteAsync
         try
         {
             var cntOUT = 0;
-            for (long i = 0; i < source.Length; i++)
+
+            while (source.Position < source.Length)
             {
                 checked
                 {
                     await source.ReadAsync(buffer);
                     {
-                        if (buffer[0]>=0x61 && buffer[0]<=0x7a)
+                        if (buffer[0] >= 0x61 && buffer[0] <= 0x7a)
                         {
                             await destination.WriteAsync(buffer);
                             cntOUT++;
@@ -40,7 +41,48 @@ public class FileReadWriteAsync
         {
             destination.Dispose();
             source.Dispose();
-            
+
         }
+    }
+
+    public Task TransferSymbols(Stream source, Stream destination)
+    {
+        return Task.Run(async () =>
+        {
+            byte[] buffer = new byte[1];
+            try
+            {
+                var cntOUT = 0;
+
+                while (source.Position < source.Length)
+                {
+                    checked
+                    {
+                        await source.ReadAsync(buffer);
+                        {
+                            if (buffer[0] >= 0x61 && buffer[0] <= 0x7a)
+                            {
+                                await destination.WriteAsync(buffer);
+                                cntOUT++;
+                            }
+                        }
+                    }
+
+
+                }
+                return cntOUT;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                destination.Dispose();
+                source.Dispose();
+
+            }
+        });
     }
 }
--- FileReadWriteAsyncTests/FileReadWriteAsyncTests.cs
+++ FileReadWriteAsyncTests/FileReadWriteAsyncTests.cs
@@ -5,6 +5,25 @@ namespace FileReadWriteAsyncTests;
 public  class FileReadWriteAsyncTests
 {
     [Fact]
+    public async Task Test_TransferSymbolsAsync()
+    {
+        //Arrange
+        Stream streamIn = new FileStream("test.raw", FileMode.Open);
+        //var lenSreamIn=streamIn.Length;
+        Stream streamOut = new FileStream("testOut.raw", FileMode.Create);
+        var ob = new FileReadWriteAsync();
+
+        //Act
+        var task = ob.TransferSymbolsAsync(streamIn, streamOut);
+        await task;
+        //Assert
+        Assert.False(streamIn.CanRead);
+        Assert.False(streamOut.CanWrite);
+        Assert.True(task.IsCompletedSuccessfully);
+        //Assert.Equal(lenSreamIn, task.Result);
+    }
+
+    [Fact]
     public async Task Test_TransferSymbols()
     {
         //Arrange
